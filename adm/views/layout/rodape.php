485d48e5d7b92820278ec29274407790d8aa4d51
Leonardo Rocha <leoheiro@hotmail.com>
2021-04-20 10:31:56 -0300

    arquivos back

---
+++ adm/views/layout/rodape.php
@@ -0,0 +1,9 @@
+
+    <footer>
+        <p>Pizzaria Byte</p>
+        <p>Backoffice</p>
+        <p>Administração</p>
+        </p>
+    </footer>
+</body>
+</html>
---
+++ adm/views/layout/topo.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Pizzaria Byte - Back Office</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Site da Pizzaria">
+    <meta name="keywords" content="Pizza, Pedido online"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/estilos.css">
+</head>
+
+<body>
+    <header>
+        <h1>Pizza Byte</h1>
+        <p>Área de Administração</p>
+        <span id="showMenu" onclick="showMenu()">&equiv; Menu</span>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="saborController.php">Sabores</a></li>
+            <li><a href="#">Clientes</a></li>
+            <li><a href="#">Pedidos</a></li>
+        </ul>
+    </nav>
